2d49c2146d01b6d1dd12f32265fec20176eb0f36
Jorge Santos <jorge.santos@endava.com>
2019-02-21 13:43:11 -0500

    remove file

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./routes');
 const app = express();
 const port = 3000;
 
--- routes/index.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-
-router.get('/', require('./01-settings'));
-
-module.exports = router;
